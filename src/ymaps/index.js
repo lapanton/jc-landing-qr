420598f455938d76b92644f35e4304c2e0d5189f
Anton Lapin <anton.lapin@jivygroup.com>
2022-03-23 20:59:11 +0200

    Add new changes

--- src/ymaps/index.js
+++ src/ymaps/index.js
@@ -1,10 +1,13 @@
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
-
+// https://yandex.ru/dev/maps/jsapi/doc/2.1/ref/reference/Placemark.html
 export const Ymaps = () => (
   <YMaps>
     <div style={{ filter: "invert(100%)" }}>
       <Map defaultState={{ center: [55.681712, 37.516449], zoom: 15}} width="100%" height="400px" >
-        <Placemark geometry={[55.681712, 37.516449]} />
+        <Placemark
+          geometry={[55.681712, 37.516449]}
+          properties={{iconCaption: "JewelCocktail"}}
+        />
       </Map>
     </div>
   </YMaps>
