b56d02093d8b09566a8399a381fa3186fb1676ba
Anton Lapin <anton.lapin@jivygroup.com>
2022-08-01 18:57:58 +0300

    Update video on desktop

--- src/shop/second/styles-second.js
+++ src/shop/second/styles-second.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   position: relative;
   z-index: 999999999;
-  max-width: 768px;
+  max-width: 480px;
   margin: 40px auto 0 auto;
   @media screen and (max-width: 768px) {
     margin: 0 auto;
--- src/shop/showHideSection/prev/styles-second.js
+++ src/shop/showHideSection/prev/styles-second.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   position: relative;
   z-index: 999999999;
-  max-width: 768px !important;
+  max-width: 480px !important;
   margin: 0 auto;
 `;
