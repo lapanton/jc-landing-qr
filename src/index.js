ffff6142ef918f7f130f2dec98ec2f0b1f4d3dcb
Anton Lapin <anton.lapin@jivygroup.com>
2023-03-17 10:26:07 +0200

    Update lang logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { IntlProvider } from "react-intl";
 import { App } from './App';
 
 
-let initLocale = "ru";
+const initLocale = () => {
+  if (navigator.language.substr(0, 2) === "ru" && !window.location.href.includes("https://jewelcocktail.com/i/")) {
+    return "ru";
+  } else {
+    return "en"
+  }
+};
 
 const loadMessages = (locale) => {
   switch (locale) {
@@ -23,40 +29,10 @@ const LocalizationWrapper = () => {
   const [locale, setLocale] = useState(initLocale);
   const [messages, setMessages] = useState(null);
 
-  // получаем IP-адрес пользователя
-  const userIP = async () => {
-    const response = await fetch('https://api.ipify.org?format=json');
-    const data = await response.json();
-    return data.ip;
-  };
-
-// определяем регион по IP
-  const getUserRegion = useCallback(async () => {
-    const ip = await userIP();
-    const response = await fetch(`https://ipinfo.io/${ip}?token=77e7d0b7132fbc`);
-    const data = await response.json();
-    const cisCountries = ['RU', 'UA', 'BY', 'KZ', 'KG', 'TJ', 'TM', 'UZ', 'AM', 'AZ', 'MD'];
-    if (cisCountries.includes(data.country)) {
-      localStorage.setItem('set_language', 'ru');
-      setLocale("ru")
-      return  "ru";
-    } else {
-      localStorage.setItem('set_language', 'en');
-      setLocale("en")
-      return "en";
-    }
-  }, []);
-
   useEffect(() => {
-    if (localStorage.getItem('set_language')) {
-      loadMessages(locale).then(setMessages);
-    } else {
-      getUserRegion().then((lang) => {
-        loadMessages(lang).then(setMessages);
-        setLocale(lang);
-      })
-    }
-  }, [locale, getUserRegion]);
+    loadMessages(locale).then(setMessages);
+  }, [locale]);
+
 
   return messages ? (
     <IntlProvider locale={locale} messages={messages}>
